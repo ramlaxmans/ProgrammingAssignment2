a0ea48832fa140ad4ffca53ef9fc531191348a17
Ramalakshmanan S <ramlaxmans@gmail.com>
2015-07-27 02:34:46 +0530

    Revised version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function makeCacheMatrix creates a matrix Object for calculating
+## its inverse. Require that Matrix X is a squre invertible matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix() , nrows=3,ncols=3){
+    x<- list(a=matrix(1:9, nrows,ncols))
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve computes the inverse of the given matrix. 
+## In case inverse matrix is already availabe, it retrieves the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    y<-solve(x)  ## Return a matrix that is the inverse of 'x'
 }
